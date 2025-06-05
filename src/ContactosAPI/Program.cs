913e88d30254c80bdec255f679492a47e07ba18c
Giorgio Gortaire <36708547+gortaire@users.noreply.github.com>
2025-06-05 17:43:22 +0000

    Comentarios eliminados

--- src/ContactosAPI/Program.cs
+++ src/ContactosAPI/Program.cs
@@ -40,5 +40,4 @@ record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
 
-// Comentario para actualizar repo
 public partial class Program { }
