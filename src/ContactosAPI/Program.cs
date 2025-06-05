e9a957ba8b4214b6afa216eb16f7edf858dc9d76
Giorgio Gortaire <36708547+gortaire@users.noreply.github.com>
2025-06-05 17:34:28 +0000

    Cambios a mi repo

--- src/ContactosAPI/Program.cs
+++ src/ContactosAPI/Program.cs
@@ -40,4 +40,5 @@ record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
 
+// Comentario para actualizar repo
 public partial class Program { }
